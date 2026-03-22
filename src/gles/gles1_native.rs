f03aaffba5400982403c48a398350fc79e6d355a
tapochiff <119063036+nekono48@users.noreply.github.com>
2026-03-22 23:59:41 +0200

    Update gles1_native.rs

--- src/gles/gles1_native.rs
+++ src/gles/gles1_native.rs
@@ -878,6 +878,7 @@ impl GLES for GLES1Native<'_> {
             )
         }
     }
+    
     unsafe fn CompressedTexImage2D(
         &mut self,
         target: GLenum,
@@ -907,10 +908,7 @@ impl GLES for GLES1Native<'_> {
             return;
         }
 
-        // OES_compressed_paletted_texture is in the common profile of OpenGL ES
-        // 1.1, so we can reasonably assume it's supported.
-        if PalettedTextureFormat::get_info(internalformat).is_none() {
-                // OES_compressed_paletted_texture is in the common profile of 
+        // OES_compressed_paletted_texture is in the common profile of 
         // OpenGL ES 1.1, so we can reasonably assume it's supported.
         let is_paletted = PalettedTextureFormat::get_info(internalformat).is_some();
         
@@ -945,50 +943,6 @@ impl GLES for GLES1Native<'_> {
                 data.as_ptr() as *const _,
             );
 
-                // If it's something else entirely, we still log it 
-                // instead of panicking
-                log_err!("Unknown CompressedTexImage2D format: {:#x}", 
-                         internalformat);
-            }
-        } else {
-            log_dbg!("Directly supported texture format: {:#x}", internalformat);
-        }
-
-        // RouteCompTexGles
-        if self.is_gles2 {
-            touchHLE_gl_bindings::gles20::PixelStorei(gles11::UNPACK_ALIGNMENT, 1);
-            
-            // Note: If your PC GPU doesn't support ATC, the texture might 
-            // be black/corrupt, but the game will NO LONGER CRASH.
-            touchHLE_gl_bindings::gles20::CompressedTexImage2D(
-                target,
-                level,
-                internalformat,
-                width,
-                height,
-                border,
-                image_size,
-                data.as_ptr() as *const _,
-            );
-        }
-
-        // RouteCompTexGles
-        if self.is_gles2 {
-            touchHLE_gl_bindings::gles20::PixelStorei(gles11::UNPACK_ALIGNMENT, 1);
-            
-            .
-            touchHLE_gl_bindings::gles20::CompressedTexImage2D(
-                target,
-                level,
-                internalformat,
-                width,
-                height,
-                border,
-                image_size,
-                data.as_ptr() as *const _,
-            );
-        }
-
             // SafeDefaultFilter
             let p_target = if (0x8515..=0x851A).contains(&target) {
                 0x8513
@@ -1013,6 +967,7 @@ impl GLES for GLES1Native<'_> {
             );
         }
     }
+
     // RouteCopyTex
     unsafe fn CopyTexImage2D(
         &mut self,
@@ -1593,7 +1548,5 @@ impl GLES for GLES1Native<'_> {
     unsafe fn MapBufferOES(&mut self, target: GLenum, access: GLenum) -> *mut GLvoid {
         gles11::MapBufferOES(target, access)
     }
-    unsafe fn UnmapBufferOES(&mut self, target: GLenum) -> GLboolean {
-        gles11::UnmapBufferOES(target)
-    }
-}
+    unsafe fn UnmapBufferOES(&mut self, target: GLenum) -> GL
+
