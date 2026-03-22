c542e7f3a8842382a60b994a3b148ee52c345e6b
tapochiff <119063036+nekono48@users.noreply.github.com>
2026-03-22 20:06:46 +0200

    Update mod.rs

--- src/frameworks/foundation/mod.rs
+++ src/frameworks/foundation/mod.rs
@@ -4,39 +4,170 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-//! Foundation framework.
+//! The Foundation framework.
 
-pub mod ns_array;
-pub mod ns_bundle;
-pub mod ns_data;
-pub mod ns_dictionary;
-pub mod ns_error;
-pub mod ns_number; // Added this line
-pub mod ns_string;
-pub mod ns_url;
-pub mod ns_value;
-
-use crate::dyld::FunctionExports;
-use crate::objc::SelectorMap;
+use crate::dyld::{export_c_func, FunctionExports};
+use crate::objc::id;
 use crate::Environment;
 
-/// Register all Foundation classes and functions.
-pub fn install(env: &mut Environment, selectors: &mut SelectorMap) {
-    // Register classes
-    ns_array::register_class(env, selectors);
-    ns_bundle::register_class(env, selectors);
-    ns_data::register_class(env, selectors);
-    ns_dictionary::register_class(env, selectors);
-    ns_error::register_class(env, selectors);
-    ns_number::register_class(env, selectors); // Added this line
-    ns_string::register_class(env, selectors);
-    ns_url::register_class(env, selectors);
-    ns_value::register_class(env, selectors);
+pub mod _nib_archive_decoder;
+pub mod ns_array;
+pub mod ns_autorelease_pool;
+pub mod ns_bundle;
+pub mod ns_character_set;
+pub mod ns_coder;
+pub mod ns_data;
+pub mod ns_date;
+pub mod ns_date_formatter;
+pub mod ns_dictionary;
+pub mod ns_enumerator;
+pub mod ns_error;
+pub mod ns_exception;
+pub mod ns_file_handle;
+pub mod ns_file_manager;
+pub mod ns_keyed_archiver;
+pub mod ns_keyed_unarchiver;
+pub mod ns_locale;
+pub mod ns_lock;
+pub mod ns_log;
+pub mod ns_notification;
+pub mod ns_notification_center;
+pub mod ns_null;
+pub mod ns_number;
+pub mod ns_objc_runtime;
+pub mod ns_object;
+pub mod ns_process_info;
+pub mod ns_property_list_serialization;
+pub mod ns_run_loop;
+pub mod ns_scanner;
+pub mod ns_set;
+pub mod ns_string;
+pub mod ns_thread;
+pub mod ns_time_zone;
+pub mod ns_timer;
+pub mod ns_url;
+pub mod ns_url_connection;
+pub mod ns_url_request;
+pub mod ns_user_defaults;
+pub mod ns_value;
+pub mod ns_xml_parser;
+
+pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
+    path: "/System/Library/Frameworks/Foundation.framework/Foundation",
+    aliases: &[],
+    class_exports: &[
+        _nib_archive_decoder::CLASSES,
+        ns_array::CLASSES,
+        ns_autorelease_pool::CLASSES,
+        ns_bundle::CLASSES,
+        ns_character_set::CLASSES,
+        ns_coder::CLASSES,
+        ns_data::CLASSES,
+        ns_date::CLASSES,
+        ns_date_formatter::CLASSES,
+        ns_dictionary::CLASSES,
+        ns_enumerator::CLASSES,
+        ns_error::CLASSES,
+        ns_file_handle::CLASSES,
+        ns_file_manager::CLASSES,
+        ns_keyed_archiver::CLASSES,
+        ns_keyed_unarchiver::CLASSES,
+        ns_locale::CLASSES,
+        ns_lock::CLASSES,
+        ns_notification::CLASSES,
+        ns_notification_center::CLASSES,
+        ns_null::CLASSES,
+        ns_number::CLASSES,
+        ns_object::CLASSES,
+        ns_process_info::CLASSES,
+        ns_property_list_serialization::CLASSES,
+        ns_run_loop::CLASSES,
+        ns_scanner::CLASSES,
+        ns_set::CLASSES,
+        ns_string::CLASSES,
+        ns_thread::CLASSES,
+        ns_timer::CLASSES,
+        ns_time_zone::CLASSES,
+        ns_url::CLASSES,
+        ns_url_connection::CLASSES,
+        ns_url_request::CLASSES,
+        ns_user_defaults::CLASSES,
+        ns_value::CLASSES,
+        ns_xml_parser::CLASSES,
+    ],
+    constant_exports: &[
+        ns_error::CONSTANTS,
+        ns_exception::CONSTANTS,
+        ns_file_manager::CONSTANTS,
+        ns_keyed_unarchiver::CONSTANTS,
+        ns_locale::CONSTANTS,
+        ns_run_loop::CONSTANTS,
+    ],
+    function_exports: &[
+        FUNCTIONS,
+        ns_exception::FUNCTIONS,
+        ns_file_manager::FUNCTIONS,
+        ns_log::FUNCTIONS,
+        ns_objc_runtime::FUNCTIONS,
+    ],
+};
+
+#[derive(Default)]
+pub struct State {
+    pub ns_autorelease_pool: ns_autorelease_pool::State,
+    pub ns_bundle: ns_bundle::State,
+    pub ns_file_manager: ns_file_manager::State,
+    pub ns_locale: ns_locale::State,
+    pub ns_notification_center: ns_notification_center::State,
+    pub ns_null: ns_null::State,
+    pub ns_process_info: ns_process_info::State,
+    pub ns_run_loop: ns_run_loop::State,
+    pub ns_string: ns_string::State,
+    pub ns_thread: ns_thread::State,
+    pub ns_user_defaults: ns_user_defaults::State,
 }
 
-/// Exported C functions for the Foundation framework.
-pub const FUNCTIONS: FunctionExports = &[
-    // Usually includes things like NSLog, NSClassFromString, etc.
-    // Ensure this list matches your current version's requirements.
-];
+pub type NSInteger = i32;
+pub type NSUInteger = u32;
+pub const NSNotFound: i32 = 0x7fffffff;
 
+#[derive(Debug)]
+#[repr(C, packed)]
+pub struct NSRange {
+    pub location: NSUInteger,
+    pub length: NSUInteger,
+}
+unsafe impl crate::mem::SafeRead for NSRange {}
+crate::abi::impl_GuestRet_for_large_struct!(NSRange);
+
+impl crate::abi::GuestArg for NSRange {
+    const REG_COUNT: usize = 2;
+    fn from_regs(regs: &[u32]) -> Self {
+        NSRange {
+            location: crate::abi::GuestArg::from_regs(&regs[0..1]),
+            length: crate::abi::GuestArg::from_regs(&regs[1..2]),
+        }
+    }
+    fn to_regs(self, regs: &mut [u32]) {
+        self.location.to_regs(&mut regs[0..1]);
+        self.length.to_regs(&mut regs[1..2]);
+    }
+}
+
+fn NSStringFromRange(env: &mut Environment, range: NSRange) -> id {
+    let loc = range.location;
+    let len = range.length;
+    let string = format!("{{{loc}, {len}}}");
+    ns_string::from_rust_string(env, string)
+}
+
+pub type NSComparisonResult = NSInteger;
+pub const NSOrderedAscending: NSComparisonResult = -1;
+pub const NSOrderedSame: NSComparisonResult = 0;
+pub const NSOrderedDescending: NSComparisonResult = 1;
+pub type NSTimeInterval = f64;
+
+#[allow(non_camel_case_types)]
+pub type unichar = u16;
+
+const FUNCTIONS: FunctionExports = &[export_c_func!(NSStringFromRange(_))];
