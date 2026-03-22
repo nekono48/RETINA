38c84af200861caff0a13100bfe875f2efe52850
tapochiff <119063036+nekono48@users.noreply.github.com>
2026-03-22 20:03:07 +0200

    Update foundation.rs

--- src/frameworks/foundation.rs
+++ src/frameworks/foundation.rs
@@ -3,13 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
 //! The Foundation framework.
-//!
-//! A concept that Foundation really likes is "class clusters": abstract classes
-//! with private concrete implementations. Apple has their own explanation of it
-//! in [Cocoa Core Competencies](https://developer.apple.com/library/archive/documentation/General/Conceptual/DevPedia-CocoaCore/ClassCluster.html).
-//! Being aware of this concept will make common types like `NSArray` and
-//! `NSString` easier to understand.
 
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::objc::id;
@@ -38,6 +33,7 @@ pub mod ns_log;
 pub mod ns_notification;
 pub mod ns_notification_center;
 pub mod ns_null;
+pub mod ns_number; // Added
 pub mod ns_objc_runtime;
 pub mod ns_object;
 pub mod ns_process_info;
@@ -81,6 +77,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         ns_notification::CLASSES,
         ns_notification_center::CLASSES,
         ns_null::CLASSES,
+        ns_number::CLASSES, // Added
         ns_object::CLASSES,
         ns_process_info::CLASSES,
         ns_property_list_serialization::CLASSES,
@@ -117,23 +114,22 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
 
 #[derive(Default)]
 pub struct State {
-    ns_autorelease_pool: ns_autorelease_pool::State,
-    ns_bundle: ns_bundle::State,
-    ns_file_manager: ns_file_manager::State,
-    ns_locale: ns_locale::State,
-    ns_notification_center: ns_notification_center::State,
-    ns_null: ns_null::State,
-    ns_process_info: ns_process_info::State,
-    ns_run_loop: ns_run_loop::State,
-    ns_string: ns_string::State,
-    ns_thread: ns_thread::State,
-    ns_user_defaults: ns_user_defaults::State,
+    pub ns_autorelease_pool: ns_autorelease_pool::State,
+    pub ns_bundle: ns_bundle::State,
+    pub ns_file_manager: ns_file_manager::State,
+    pub ns_locale: ns_locale::State,
+    pub ns_notification_center: ns_notification_center::State,
+    pub ns_null: ns_null::State,
+    pub ns_process_info: ns_process_info::State,
+    pub ns_run_loop: ns_run_loop::State,
+    pub ns_string: ns_string::State,
+    pub ns_thread: ns_thread::State,
+    pub ns_user_defaults: ns_user_defaults::State,
 }
 
 pub type NSInteger = i32;
 pub type NSUInteger = u32;
 
-// this should be equal to NSIntegerMax
 pub const NSNotFound: i32 = 0x7fffffff;
 
 #[derive(Debug)]
@@ -171,24 +167,10 @@ pub const NSOrderedAscending: NSComparisonResult = -1;
 pub const NSOrderedSame: NSComparisonResult = 0;
 pub const NSOrderedDescending: NSComparisonResult = 1;
 
-/// Number of seconds.
 pub type NSTimeInterval = f64;
 
-/// UTF-16 code unit.
 #[allow(non_camel_case_types)]
 pub type unichar = u16;
 
-/// Utility to help with implementing the `hash` method, which various classes
-/// in Foundation have to do.
-fn hash_helper<T: std::hash::Hash>(hashable: &T) -> NSUInteger {
-    use std::hash::Hasher;
-
-    // Rust documentation says DefaultHasher::new() should always return the
-    // same instance, so this should give consistent hashes.
-    let mut hasher = std::collections::hash_map::DefaultHasher::new();
-    hashable.hash(&mut hasher);
-    let hash_u64: u64 = hasher.finish();
-    (hash_u64 as u32) ^ ((hash_u64 >> 32) as u32)
-}
-
 const FUNCTIONS: FunctionExports = &[export_c_func!(NSStringFromRange(_))];
+
