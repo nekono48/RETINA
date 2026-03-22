6c13f6ef44c9c95637112e424aac599b000e81dd
tapochiff <119063036+nekono48@users.noreply.github.com>
2026-03-23 00:04:05 +0200

    Update gles1_native.rs

--- src/gles/gles1_native.rs
+++ src/gles/gles1_native.rs
@@ -1549,4 +1549,17 @@ impl GLES for GLES1Native<'_> {
         gles11::MapBufferOES(target, access)
     }
     unsafe fn UnmapBufferOES(&mut self, target: GLenum) -> GL
-
+            unsafe fn GetBufferParameteriv(&mut self, target: GLenum, pname: GLenum, params: *mut GLint) {
+        if self.is_gles2 {
+            touchHLE_gl_bindings::gles20::GetBufferParameteriv(target, pname, params)
+        } else {
+            gles11::GetBufferParameteriv(target, pname, params)
+        }
+    }
+    unsafe fn MapBufferOES(&mut self, target: GLenum, access: GLenum) -> *mut GLvoid {
+        gles11::MapBufferOES(target, access)
+    }
+    unsafe fn UnmapBufferOES(&mut self, target: GLenum) -> GLboolean {
+        gles11::UnmapBufferOES(target)
+    }
+} // Закрывает impl GLES for GLES1Native
