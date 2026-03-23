6ceaa5b8446093229f0185776588cbe17eacd085
tapochiff <119063036+nekono48@users.noreply.github.com>
2026-03-23 10:19:59 +0200

    Update gles1_native.rs

--- src/gles/gles1_native.rs
+++ src/gles/gles1_native.rs
@@ -1363,19 +1363,26 @@ impl GLES for GLES1Native<'_> {
         }
     }
 
-        unsafe fn DeleteFramebuffersOES(&mut self, n: GLsizei, framebuffers: *const GLuint) {
+            unsafe fn DeleteRenderbuffersOES(&mut self, n: GLsizei, renderbuffers: *const GLuint) {
         if self.is_gles2 {
-            touchHLE_gl_bindings::gles20::DeleteFramebuffers(n, framebuffers);
+            touchHLE_gl_bindings::gles20::DeleteRenderbuffers(n, renderbuffers);
         } else {
-            gles11::DeleteFramebuffersOES(n, framebuffers);
+            gles11::DeleteRenderbuffersOES(n, renderbuffers);
         }
     }
 
-    unsafe fn MapBufferOES(&mut self, target: GLenum, access: GLenum) -> *mut GLvoid {
-        gles11::MapBufferOES(target, access)
+    unsafe fn GenerateMipmapOES(&mut self, target: GLenum) {
+        if self.is_gles2 {
+            touchHLE_gl_bindings::gles20::GenerateMipmap(target);
+        } else {
+            gles11::GenerateMipmapOES(target);
+        }
     }
 
-    unsafe fn UnmapBufferOES(&mut self, target: GLenum) -> GLboolean {
-        gles11::UnmapBufferOES(target)
+    unsafe fn GetBufferParameteriv(&mut self, target: GLenum, pname: GLenum, params: *mut GLint) {
+        if self.is_gles2 {
+            touchHLE_gl_bindings::gles20::GetBufferParameteriv(target, pname, params);
+        } else {
+            gles11::GetBufferParameteriv(target, pname, params);
+        }
     }
-}
